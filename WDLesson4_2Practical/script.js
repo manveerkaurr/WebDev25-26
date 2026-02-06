4e4bde21e08e629703585901dc136f5d0fec9eb0
manveerkaurr <manveerk7@nycstudents.net>
2026-02-06 19:02:47 +0000

    ca

--- WDLesson4_2Practical/script.js
+++ WDLesson4_2Practical/script.js
@@ -2,7 +2,8 @@
 /*
 function name(){
   // Step 1: Create variables for the information from the text inputs and the output element
-
+  
+ 
   // Step 2: Complete the process
   //    a) Perform any calculations necessary
   //    b) Perform necessary decisions
@@ -13,13 +14,21 @@ function name(){
 */
 
 /* Challenge 2:  Create the function for Challenge 1 scenario */
-
-
+function area() {
+  length = parseFloat(document.getElementById('length').value);
+  width = parseFloat(document.getElementById('width').value);
+  result = document.getElementById('result');
+  if (length <= 0 || width <= 0) {
+    result.display= "Inappropriate measurement";
+  } else {
+    area = length * width;
+    result= "The area is: " + area;
+  }
 
 /* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
 1) Review the HTML for the UI.  Remember dropdown boxes are just input.
 2) You will need to write HTML as part of the output.  
    For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
 function pet(){
-
+  
 }
